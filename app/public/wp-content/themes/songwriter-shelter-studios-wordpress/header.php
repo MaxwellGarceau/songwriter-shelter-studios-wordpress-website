b05f97e38100211ef214e5ca4d43f281174b0939
Max Garceau <maxwellgarceau@gmail.com>
2018-05-23 09:50:27 -0500

    Fixed favicon caching problem. Updated meta tag information to make it dynamic.

--- app/public/wp-content/themes/songwriter-shelter-studios-wordpress/header.php
+++ app/public/wp-content/themes/songwriter-shelter-studios-wordpress/header.php
@@ -4,21 +4,16 @@
 <head>
     <meta http-equiv="X-UA-Compatible" content="IE=11,chrome=1" />
     <meta charset="<?php bloginfo(" charset "); ?>"/>
-    <!-- <title>The Songwriter Shelter Recording Studios | Max Garceau</title> -->
-    <!-- <meta name="generator" content="Wix.com Website Builder"> -->
     <meta name="fb_admins_meta_tag" content="" />
     <meta name="keywords" content="Arranging, Audio, Engineer, Mastering, Metal, Mixing, Music, Recording, Rock, Studio" />
     <meta name="description" content="The Songwriter Shelter Recording Studios is a music writing, recording and production company based out of Nashville, TN. Contact Max Garceau today for a quote." />
-    <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.ico" type="image/x-icon" />
-    <link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/apple-touch-icon.png" type="image/png" />
-    <!--     <meta http-equiv="X-Wix-Meta-Site-Id" content="3d58b758-817c-4ab8-ae6f-cb7853448b13" />
-    <meta http-equiv="X-Wix-Application-Instance-Id" content="c4d8f195-dd27-4ac6-989b-898635bdea80" />
-    <meta http-equiv="X-Wix-Published-Version" content="141" /> -->
+    <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri();?>/images/favicon.ico?r=12523" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri();?>/images/apple-touch-icon.png" type="image/png" />
     <meta http-equiv="etag" content="dcc504720ff6a9ebac1ef56d5ea7cb7e" />
     <meta property="og:title" content="The Songwriter Shelter Recording Studios | Max Garceau" />
     <meta property="og:type" content="website" />
-    <meta property="og:url" content="https://www.songwritershelterstudios.com/" />
-    <meta property="og:image" content="https://static.wixstatic.com/media/6a1d9d_f59dc084a5804bdc9a3f64c2fa420bc5%7Emv2.png" />
+    <meta property="og:url" content="<?php echo site_url();?>" />
+    <meta property="og:image" content="<?php echo site_url();?>/wp-content/uploads/2018/04/sss.png" />
     <meta property="og:site_name" content="The Songwriter Shelter Recording Studios | Max Garceau" />
     <meta property="og:description" content="The Songwriter Shelter Recording Studios is a music writing, recording and production company based out of Nashville, TN. Contact Max Garceau today for a quote." />
     <meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE" />
